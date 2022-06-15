39c0a40cabb3d1658df436f3724a78287fe507e2
d4daveshep <david@daveshep.net.nz>
2022-06-16 09:15:48 +1200

    PCAP Module 2

--- string_functions.py
+++ string_functions.py
@@ -3,5 +3,18 @@
 char_1 = 'a'
 char_2 = ' '  # space
 
+# print the code point number from the char
 print(ord(char_1))
 print(ord(char_2))
+
+print()
+# Demonstrating the chr() function.
+# print the char from the code piont number
+print(chr(97))
+print(chr(945))
+
+x = 'a'
+print(chr(ord(x)) == x)
+
+y = 97
+print(ord(chr(y)) == y)
