2bc1464679c62a85dbd04109cf838e49373606cf
d4daveshep <david@daveshep.net.nz>
2022-06-24 15:06:12 +1200

    added docstring for triangle.py

--- triangle.py
+++ triangle.py
@@ -6,11 +6,18 @@ from point import Point
 
 
 class Triangle:
+    """
+    Triangle is a composition of 3 Point objects.
+    """
 
     def __init__(self, point1, point2, point3):
         self.__vertices = [point1, point2, point3]
 
     def perimeter(self):
+        """
+        perimeter is calculated using the Point.distance_from_point() method
+        :return: length of the triangle sides
+        """
         len_side1 = self.__vertices[0].distance_from_point(self.__vertices[1])
         len_side2 = self.__vertices[1].distance_from_point(self.__vertices[2])
         len_side3 = self.__vertices[0].distance_from_point(self.__vertices[2])
